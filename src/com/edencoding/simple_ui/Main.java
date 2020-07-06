0aee77fd93ebec2812d5797c8dfad20c8796626c
Ed Eden-Rump <ed.edenrump@gmail.com>
2020-07-06 20:37:04 +0100

    add simple-ui project files

---
+++ src/com/edencoding/simple_ui/Main.java
@@ -0,0 +1,46 @@
+package com.edencoding.simple_ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    private double xOffset;
+    private double yOffset;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+            event.consume();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+            event.consume();
+        });
+
+        Scene scene = new Scene(root);
+        scene.getRoot().setEffect(new DropShadow(10, Color.rgb(100,100,100)));
+        scene.setFill(Color.TRANSPARENT);
+
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
