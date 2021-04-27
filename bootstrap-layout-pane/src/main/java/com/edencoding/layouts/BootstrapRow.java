a5c5e30777f37941b2f06d5d817fe943286642d7
Ed Eden-Rump <ed.edenrump@gmail.com>
2021-04-27 19:54:25 +0100

    add column-control methods to BootstrapRow

--- bootstrap-layout-pane/src/main/java/com/edencoding/layouts/BootstrapRow.java
+++ bootstrap-layout-pane/src/main/java/com/edencoding/layouts/BootstrapRow.java
@@ -1,8 +1,26 @@
 package com.edencoding.layouts;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BootstrapRow {
 
-    public void addColumn(BootstrapColumn column){
+    List<BootstrapColumn> columns = new ArrayList<>();
 
+    public void addColumn(BootstrapColumn column){
+        columns.add(column);
+    }
+
+    public void removeColumn(BootstrapColumn column){
+        columns.remove(column);
+    }
+
+    public void clear(){
+        columns.clear();
+    }
+
+    public List<BootstrapColumn> getColumns(){
+        return Collections.unmodifiableList(columns);
     }
 }
