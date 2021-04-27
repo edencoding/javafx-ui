6a54fe7e56e188813d680ea72aa1c5f2feec8b49
Ed Eden-Rump <ed.edenrump@gmail.com>
2021-04-27 18:58:05 +0100

    refactor column

--- bootstrap-layout-pane/src/main/java/com/edencoding/layouts/BootstrapColumn.java
+++ bootstrap-layout-pane/src/main/java/com/edencoding/layouts/BootstrapColumn.java
@@ -7,27 +7,16 @@ public class BootstrapColumn {
 
     Node content;
 
-    private int col = 1;
-    private int colSm = -1;
-    private int colMd = -1;
-    private int colLg = -1;
-    private int colXL = -1;
+    int[] columnWidths = new int[]{
+            1,  //XS (default)
+            -1, //Sm
+            -1, //Md
+            -1, //Lg
+            -1  //XL
+    };
 
     public BootstrapColumn(Node content) {
         this.content = content;
-        percolate();
-    }
-
-    /**
-     * Propagate column break points based on a copy-smaller biased approach
-     * This replicates the min-width media query approach addopted by Bootstrap
-     */
-    private void percolate() {
-        col = MathUtils.clamp(col, 1, 12); //ensure col is valid before percolating
-        if (!isValid(colSm)) colSm = col;
-        if (!isValid(colMd)) colMd = colSm;
-        if (!isValid(colLg)) colLg = colMd;
-        if (!isValid(colXL)) colXL = colLg;
     }
 
     /**
@@ -47,24 +36,7 @@ public class BootstrapColumn {
      * @param width      the requested width at this breakpoint (must be between 1 and 12);
      */
     public void setBreakpointColumnWidth(BootstrapColumnBreakPoint breakPoint, int width) {
-        switch (breakPoint) {
-            case XSMALL:
-                col = MathUtils.clamp(width, 1, 12);
-                break;
-            case SMALL:
-                colSm = MathUtils.clamp(width, 1, 12);
-                break;
-            case MEDIUM:
-                colMd = MathUtils.clamp(width, 1, 12);
-                break;
-            case LARGE:
-                colLg = MathUtils.clamp(width, 1, 12);
-                break;
-            case XLARGE:
-                colXL = MathUtils.clamp(width, 1, 12);
-                break;
-        }
-        percolate();
+        columnWidths[breakPoint.getValue()] = MathUtils.clamp(width, 1, 12);
     }
 
     /**
@@ -73,35 +45,20 @@ public class BootstrapColumn {
      * @param breakPoint the breakpoint to reset
      */
     public void unsetBreakPoint(BootstrapColumnBreakPoint breakPoint) {
-        switch (breakPoint) {
-            case XSMALL:
-                col = 1;
-                break;
-            case SMALL:
-                colSm = -1;
-                break;
-            case MEDIUM:
-                colMd = -1;
-                break;
-            case LARGE:
-                colLg = -1;
-                break;
-            case XLARGE:
-                colXL = -1;
-                break;
-        }
-        percolate();
+        columnWidths[breakPoint.getValue()] = -1;
     }
 
     /**
      * Reset all column width break points, so the default width at all break points is 1.
      */
     public void unsetAllBreakPoints() {
-        this.col = 1;
-        this.colSm = -1;
-        this.colMd = -1;
-        this.colLg = -1;
-        this.colXL = -1;
+        this.columnWidths = new int[]{
+                1,  //XS (default)
+                -1, //Sm
+                -1, //Md
+                -1, //Lg
+                -1  //XL
+        };
     }
 
     /**
@@ -112,11 +69,10 @@ public class BootstrapColumn {
      * @return the requested width at that breakpoint, or based on a lower breakpoint if the specified bp has not been set.
      */
     public int getColumnWidth(BootstrapColumnBreakPoint breakPoint) {
-        int[] breakPoints = new int[]{col, colSm, colMd, colLg, colXL};
 
         //Iterate through breakpoints, beginning at the specified bp, travelling down. Return first valid bp value.
         for (int i = breakPoint.getValue(); i > 0; i--) {
-            if (isValid(breakPoints[i])) return breakPoints[i];
+            if (isValid(columnWidths[i])) return columnWidths[i];
         }
 
         //If none are valid, return 1
